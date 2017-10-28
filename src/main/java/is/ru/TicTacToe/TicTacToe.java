a5615bff713911ca6570ff056f3ec2c6e6770094
RegAse <brucejackie21@gmail.com>
2017-10-28 01:36:31 +0000

    Added CreateNewGame to start a TicTacToe game and added unit tests to check if game has started.

--- src/main/java/is/ru/TicTacToe/TicTacToe.java
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -2,8 +2,20 @@ package is.ru.TicTacToe;
 
 public class TicTacToe {
 
+    public boolean isStarted = false;
+
+    private Grid gameGrid;
+
+    public void CreateNewGame() {
+        gameGrid = new Grid();
+        isStarted = true;
+    }
+
     public static void main( String[] args ) {
         System.out.println( "Welcome to TicTacToe" );
+        TicTacToe game = new TicTacToe();
+        game.CreateNewGame();
+
         System.exit( 0 ); //success
     }
 
--- src/test/java/is/ru/TicTacToe/TicTacToeTest.java
+++ src/test/java/is/ru/TicTacToe/TicTacToeTest.java
@@ -1,4 +1,21 @@
 package is.ru.TicTacToe;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class TicTacToeTest {
+
+    @Test
+    public void checkIfTicTacToeGameThatHasNotBeenStartedTest() {
+        TicTacToe ttt = new TicTacToe();
+        Assert.assertEquals(ttt.isStarted, false);
+    }
+
+    @Test
+    public void checkIfTicTacToeGameThatHasBeenStartedTest() {
+        TicTacToe ttt = new TicTacToe();
+        ttt.CreateNewGame();
+        Assert.assertEquals(ttt.isStarted, true);
+    }
+
 }
