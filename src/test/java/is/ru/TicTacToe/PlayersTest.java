415e57ad91be4dbd55b9cf8d2a59fc4a9b6a0a8f
helga13 <helga13@ru.is>
2017-10-28 22:31:25 +0000

    Test successful PlayersTest

--- src/test/java/is/ru/TicTacToe/PlayersTest.java
+++ src/test/java/is/ru/TicTacToe/PlayersTest.java
@@ -1,3 +1,36 @@
 package is.ru.TicTacToe;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+
+public class PlayersTest {
+
+	@Test
+	public void testGetName()
+	{
+		Players TestPlayer = new Players(1, "Nonni");
+		assertEquals("Nonni", TestPlayer.getName());
+	}
+
+	/*@Test
+	public void testGetNumber()
+	{
+		Players TestPlayer = new Player(1, "Nonni");		
+		assertEquals(1, TestPlayer.getNumber()); 
+	}
+	
+
+	@Test
+	public void testGetSymbolForPlayerOne()
+	{
+		Players TestPlayer = new Player(1, "Nonni");
+		assertEquals("O", TestPlayer.getSymbol());
+	}
+
+    @Test
+     	public void testGetSymbolForPlayerTwo()
+        {
+            Player TestPlayer = new Player(2, "Kalli");
+            assertEquals("X", TestPlayer.getSymbol());
+        }*/
+
+}
