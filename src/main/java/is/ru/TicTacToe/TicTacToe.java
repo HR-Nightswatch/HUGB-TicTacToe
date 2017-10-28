09d768e919b9bcdda3a72bb10bda3586410cfb89
RegAse <brucejackie21@gmail.com>
2017-10-28 19:04:06 +0000

    TicTacToe asks for names and assigns players and asks if player wants to play another.

--- src/main/java/is/ru/TicTacToe/TicTacToe.java
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -1,19 +1,56 @@
 package is.ru.TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 
     public boolean isStarted = false;
 
     private Grid gameGrid;
+    private Scanner input;
 
     public TicTacToe() {
         gameGrid = new Grid();
         isStarted = true;
+        input = new Scanner(System.in);
+    }
+
+    public void welcome() {
+        System.out.println("Welcome to TicTacToe");
+    }
+
+    public Players newPlayer(int playerNumber) {
+        System.out.print("Player " + playerNumber + ": ");
+        String playerName = input.next();
+
+        return new Players(playerNumber, playerName);
+    }
+
+    public boolean askPlayerToPlayAnother() {
+        System.out.println("Do you want to play another game? (yes/no)");
+        String answer = input.next();
+        if (answer.equals("y") || answer.equals("yes")) {
+            return true;
+        }
+
+        return false;
     }
 
     public static void main( String[] args ) {
-        System.out.println( "Welcome to TicTacToe" );
         TicTacToe game = new TicTacToe();
+
+        boolean isPlaying = true;
+
+        while(isPlaying) {
+            game.welcome();
+
+            System.out.println("Enter desired player names");
+            Players player1 = game.newPlayer(1);
+            Players player2 = game.newPlayer(2);
+
+            isPlaying = game.askPlayerToPlayAnother();
+        }
+        System.out.println("Thank you for playing! :)");
 
         System.exit( 0 ); //success
     }
